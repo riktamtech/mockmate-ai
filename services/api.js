ed55b54d9d39c99ffc0ba8d6eb938545de83e3ad
JeZoS <prabhat262111@gmail.com>
2026-01-21 11:57:20 +0530

    fix: comment out local API URL for production readiness

--- services/api.js
+++ services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001';
+// const API_URL = 'http://localhost:5001';
+const API_URL = '';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
